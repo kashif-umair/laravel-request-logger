871875d3ef93e4efd17e88fd7e897ce599daecd3
Royal Wang <hxroyal@gmail.com>
2020-05-07 11:53:29 +0800

    优化配置文件

--- config/request-logger.php
+++ config/request-logger.php
@@ -1,12 +1,10 @@
 <?php
 
 return [
-    'format' => '{remote-addr} HTTP/{http-version} {method} "{fullUrl}" {status} "{user-agent}" {contentLength} {REFERER} {requestData}',
+    'format' => 'default',
     'log' => [
         'channel' => 'daily', //You can create a custom log channel
         'level' => 'info',
     ],
-    'exclude' => [
-
-    ]
+    'exclude' => []
 ];
